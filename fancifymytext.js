a7411ee995d00eed2ff8c26690be4e13878fb853
WarriorWyatt100 <98424365+WarriorWyatt100@users.noreply.github.com>
2025-10-01 11:32:35 -0700

    Update fancifymytext.js

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,3 +1,17 @@
 function makeBigger() {
     document.getElementById("userText").style.fontSize = "24pt";
 }
+
+function applyStyle() {
+    let textArea = document.getElementById("userText");
+
+    if (document.getElementById("fancy").checked) {
+        textArea.style.fontWeight = "bold";
+        textArea.style.color = "blue";
+        textArea.style.textDecoration = "underline";
+    } else {
+        textArea.style.fontWeight = "normal";
+        textArea.style.color = "black";
+        textArea.style.textDecoration = "none";
+    }
+}
